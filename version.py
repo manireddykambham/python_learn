d19411789337146eefa98d00e0acd6f7e607cd35
Mani Shankar <manishankar377@gmail.com>
2025-03-14 00:34:41 -0500

    test types

--- version.py
+++ version.py
@@ -1,3 +1,18 @@
 import sys
 
 print("Python version:", sys.version)
+if 5>2:
+    print("5 is greater than 2")
+
+x = 5
+y = 10
+if x < y:
+    print("x is less than y")
+
+X=2
+y="hello"
+z='world'
+a=1.0
+print("X is", X," and x is ", x)
+print(type(X), type(x), type(y), type(z), type(a))
+
