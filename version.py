25f59acc799a25f7c5ff7c361155c7ee79637115
Mani Shankar <manishankar377@gmail.com>
2025-03-14 00:37:16 -0500

    test list

--- version.py
+++ version.py
@@ -16,3 +16,15 @@ a=1.0
 print("X is", X," and x is ", x)
 print(type(X), type(x), type(y), type(z), type(a))
 
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+print(myvar, my_var, _my_var, myVar, MYVAR, myvar2)
+fruits = ["apple", "banana", "cherry"]#if added more in list it will thorw error
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
